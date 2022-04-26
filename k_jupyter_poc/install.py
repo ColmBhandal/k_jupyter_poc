b5d5eb9017f5d21e911796e99c9517bc937bf650
Colm Bhandal <cbhandal@guidewire.com>
2022-04-26 14:16:07 +0100

    Fixed install file
    Refactored it to be more modular
    Fixed bug - shouldn't be passing the first arg of sys.argv
    The first arg of sys.arv is the working dir (or something like that)
    So we skip this when passing to our CLI args parser

--- k_jupyter_poc/install.py
+++ k_jupyter_poc/install.py
@@ -2,9 +2,12 @@ import json
 import os
 import sys
 import argparse
+import logging
 
 from jupyter_client.kernelspec import KernelSpecManager
 from IPython.utils.tempdir import TemporaryDirectory
+
+logging.basicConfig(level = logging.INFO)
 
 kernel_json = {
     "argv": [
@@ -23,7 +26,7 @@ def install_K_kernel_spec(user=True, prefix=None):
     with TemporaryDirectory() as tempDir:
         os.chmod(tempDir, 0o755)  # Need to make the file user-readable
         with open(os.path.join(tempDir, 'kernel.json'), 'w') as jsonFile:
-            json.dump(kernel_json, jsonFile, sort_keys=True)        
+            json.dump(kernel_json, jsonFile, sort_keys=True)
 
         print('Installing the K IPython kernel')
         KernelSpecManager().install_kernel_spec(tempDir, 'k', user=user, replace=True, prefix=prefix)
@@ -35,46 +38,54 @@ def _is_root():
     except AttributeError:
         return False  # This could happen if the platform is non-Unix. In any case, assume non-root.
 
-
-def main(argv=[]):
-    parser = argparse.ArgumentParser(
+def parse_args(argv):
+    arg_parser = argparse.ArgumentParser(
         description='Install KernelSpec for K Kernel'
     )
-	
-	# Ensure that only 1 installation location is specifies
-    locationsGroup = parser.add_mutually_exclusive_group()
 
-    locationsGroup.add_argument(
+    # Ensure that only 1 installation location is specifies
+    locations_group = arg_parser.add_mutually_exclusive_group()
+
+    locations_group.add_argument(
         '--user',
         help='Install KernelSpec in user homedirectory',
         action='store_true'
     )
-    locationsGroup.add_argument(
+    locations_group.add_argument(
         '--sys-prefix',
         help='Install KernelSpec in sys.prefix. Useful in conda / virtualenv',
         action='store_true',
         dest='sys_prefix'
     )
-    locationsGroup.add_argument(
+    locations_group.add_argument(
         '--prefix',
         help='Install KernelSpec in this prefix',
         default=None
     )
 
-    args = parser.parse_args(argv)
+    args = arg_parser.parse_args(argv)
+    return args
 
-    if args.sys_prefix:
-        prefix = sys.prefix
-        user = None
-    elif args.user:
-        prefix = None
-        user = True
-    else:
-        prefix = args.prefix
-        user = None
 
-    install_K_kernel_spec(user=user, prefix=prefix)
+def main(argv=None):
+    logging.info("Installing K Jupyter POC")
 
+    try:
+      args = parse_args(argv)
+
+      if args.sys_prefix:
+          prefix = sys.prefix
+          user = None
+      elif args.user:
+          prefix = None
+          user = True
+      else:
+          prefix = args.prefix
+          user = None
+
+      install_K_kernel_spec(user=user, prefix=prefix)
+    except Exception as ex:
+      logging.exception("Exception encountered during main installation script")
 
 if __name__ == '__main__':
-    main(argv=sys.argv)
+    main(argv=sys.argv[1:])
