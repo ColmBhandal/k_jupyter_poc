f78a8fa99c679cfc763e372587fc5259d340c98e
Colm Bhandal <cbhandal@guidewire.com>
2022-04-26 17:15:23 +0100

    Buffering k fragments
    Also changed the k code pattern.
    If we're fragmenting the k definition, we can't enforce the existince of module or endmodule
    We can't even enforce one of the two - it could be an intermediate segment of code

--- k_jupyter_poc/kernel.py
+++ k_jupyter_poc/kernel.py
@@ -1,20 +1,34 @@
 from ipykernel.kernelbase import Kernel
 import subprocess
 import re
+import tempfile
+import os
 
 # TODO: Inject version in one place only
 __version__ = '0.0.0'
 
-k_code_pattern = re.compile("(?s).*module(?s).*endmodule(?s).*")
+k_code_pattern = re.compile("^//k")
+
+def kompile_and_run(k_buffer, code):
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        os.chmod(tmpdirname, 0o755) # Starts off as 700, not user readable
+    return "K: " + "\n".join(k_buffer) + "\nCode: " + code
 
 class KKernel(Kernel):
+
+    def __init__(self, **kwargs):
+        Kernel.__init__(self, **kwargs)
+        self._k_buffer = []
+
     implementation = 'k'
     implementation_version = __version__
     language = 'k'
     # TODO: Get the actual K language version somehow and inject it here, or at least hardwire the version being used
     language_version = '0.0.0'
     banner = "K kernel.\n" \
-             "Saves K modules in the background and allows them to be Kompiled / KRun"
+             '''Incrementally build a K-definition via multiple k-fragments and then run these with krun.
+                All parts of your K-definition should being with the comment "//k". All other cells will be treated as sh commands.
+             '''
 
     @property
     def language_info(self):
@@ -27,9 +41,11 @@ class KKernel(Kernel):
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
-        message = 'No k code detected\n'
         if re.match(k_code_pattern, code):
-            message = 'K code detected\n'
+            self._k_buffer.append(code)
+            message = 'K code fragment buffered.\n'
+        else:
+            message = kompile_and_run(self._k_buffer, code)
         if not silent:
             message += subprocess.run(['kompile', '--version'], check=True, capture_output=True, text=True).stdout
             stream_content = {'name': 'stdout', 'text': message}
